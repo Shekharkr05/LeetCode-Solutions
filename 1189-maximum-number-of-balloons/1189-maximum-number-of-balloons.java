77a1b468c4d8871022b8520fa96581e3f8bc803e
Shekhar kumar <shekharkr278@gmail.com>
2026-06-22 21:37:56 +0530

    Time: 2 ms (96.67%), Space: 43.5 MB (62.01%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character,Integer> map=new HashMap<>();
+      /*  HashMap<Character,Integer> map=new HashMap<>();
         for(char c:text.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
         }
@@ -12,11 +12,21 @@ class Solution {
        for(char c:t.toCharArray()){
         if((map.getOrDefault(c,0))==0)return count;
           else  map.put(c,map.get(c)-1);
-        
-        
+              
        }
        count++;
-       }
+       }*/
+          int b=0,a=0,l=0,o=0,n=0;
+          for(char c:text.toCharArray()){
+            if(c=='b')b++;
+            else if(c=='a')a++;
+            else if(c=='o')o++;
+            else if(c=='l')l++;
+            else if(c=='n')n++;
+          }
+          return  Math.min(b,Math.min(a,Math.min(o/2,Math.min(n,l/2))));
+        
+    
        
     }
 }
