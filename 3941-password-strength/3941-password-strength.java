f08bdc70b56f5cc8a673e404f8f91e1c1536007f
Shekhar kumar <shekharkr278@gmail.com>
2026-06-27 16:12:47 +0530

    Time: 16 ms (25.61%), Space: 47.1 MB (66.47%) - LeetHub

--- 3941-password-strength/3941-password-strength.java
+++ 3941-password-strength/3941-password-strength.java
@@ -1,6 +1,6 @@
 class Solution {
     public int passwordStrength(String password) {
-        int sum=0;
+       /* int sum=0;
 StringBuilder b=new StringBuilder();
         for(var i:password.toCharArray()){
             if(Character.isLetter(i)&&Character.isLowerCase(i)&&b.indexOf(i+"")==-1){
@@ -20,6 +20,18 @@ StringBuilder b=new StringBuilder();
             b.append(i);
             }
         }
-            return sum;
+            return sum;*/
+            HashSet<Character> set=new HashSet<>();
+            for(var i:password.toCharArray()){
+                set.add(i);
+            }
+           var sum=0;
+            for(var i:set){
+            if(i>='a'&&i<='z')sum+=1;
+            else if(i>='A'&&i<='Z')sum+=2;
+            else if(Character.isDigit(i))sum+=3;
+            else sum+=5;
+            }
+           return sum;
     }
 }
