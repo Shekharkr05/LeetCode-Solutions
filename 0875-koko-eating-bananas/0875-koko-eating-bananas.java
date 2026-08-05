7ff01a1cf12bcbc386c85b00e2c0e1f81fb7b358
Shekhar kumar <shekharkr278@gmail.com>
2026-08-05 09:12:28 +0530

    Time: 13 ms (42.15%), Space: 47.9 MB (45.37%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,25 +1,49 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int max=piles[0],n=piles.length;
-        for(var i=1;i<n;i++)max=Math.max(piles[i],max);
-        int l=1;
-        int r=max;
-        int ans=max;
-       while(l<=r){
-        int mid=l+(r-l)/2;
-        long totalHr=0;
-        for(int i=0;i<n;i++){
-            totalHr+=piles[i]/mid;
-            totalHr+=((piles[i]%mid)!=0)?1:0;
+      /*int max = piles[0], n = piles.length;
+        for (var i = 1; i < n; i++)
+            max = Math.max(piles[i], max);
+        int l = 1;
+        int r = max;
+        int ans = max;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long totalHr = 0;
+            for (int i = 0; i < n; i++) {
+                totalHr += piles[i] / mid;
+                totalHr += ((piles[i] % mid) != 0) ? 1 : 0;
+            }
+            if (totalHr > h)
+                l = mid + 1;
+            else {
+                r = mid - 1;
+                ans = mid;
+            }
+
         }
-        if(totalHr>h)l=mid+1;
-        else {
-            r=mid-1;
-            ans=mid;
+        return ans;*/
+
+          int max = piles[0], n = piles.length;
+        for (var i = 1; i < n; i++)
+            max = Math.max(piles[i], max);
+        int l = 1;
+        int r = max;
+       
+        while (l <r) {
+            int mid = l + (r - l) / 2;
+            long totalHr = 0;
+            for (int i = 0; i < n; i++) {
+                totalHr += piles[i] / mid;
+                totalHr += ((piles[i] % mid) != 0) ? 1 : 0;
+            }
+            if (totalHr > h)
+                l = mid + 1;
+            else {
+                r = mid;
+               
+            }
+
         }
-      
-        
-       }
-       return ans;
+        return l;
     }
 }
